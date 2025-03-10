1d483515c333481f79b6432e8a366ba886f0f073
JulioEvencio <julioigreja.dev@gmail.com>
2025-03-10 16:19:02 -0300

    version 1.5

--- dyrvania/src/dyrvania/Game.java
+++ dyrvania/src/dyrvania/Game.java
@@ -92,7 +92,7 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 		this.addKeyListener(this);
 		this.addMouseListener(this);
 
-		this.VERSION = "1.4 DEMO";
+		this.VERSION = "1.5";
 
 		this.WIDTH = 800;
 		this.HEIGHT = 450;
